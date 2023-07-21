f37ca4052596bac2aa77ba9944c3665b879ea9e1
Gris <gristran02@gmail.com>
2023-07-21 15:21:14 -0400

    add cicd.groovy

---
+++ cicd.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/multi_tailport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/multi_tailport.git'), string(name: 'PORT_DESCRIPTION', value: 'MultiTail lets you view one or multiple files like the original tail program. The difference is that it creates multiple windows on your console (with ncurses).' )]
+  }
+}
