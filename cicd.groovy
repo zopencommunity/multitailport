a2973d023bb7608db83b1e330a28a0bd3af63ba4
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:08 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/multi_tailport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/multi_tailport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/multi_tailport.git'), string(name: 'PORT_DESCRIPTION', value: 'MultiTail lets you view one or multiple files like the original tail program. The difference is that it creates multiple windows on your console (with ncurses).' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/multi_tailport.git'), string(name: 'PORT_DESCRIPTION', value: 'MultiTail lets you view one or multiple files like the original tail program. The difference is that it creates multiple windows on your console (with ncurses).' )]
   }
 }
